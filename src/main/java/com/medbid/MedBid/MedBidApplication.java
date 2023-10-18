9269f0817b85f1bf2c678bc9642a4d1bb4813058
Nika_Chekurashvili <Nika_Chekurashvili@epam.com>
2023-10-19 01:40:51 +0400

    [MedBid] Initial commit containing required dependencies and initial project

---
+++ src/main/java/com/medbid/MedBid/MedBidApplication.java
@@ -0,0 +1,13 @@
+package com.medbid.MedBid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedBidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedBidApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/medbid/MedBid/MedBidApplicationTests.java
@@ -0,0 +1,13 @@
+package com.medbid.MedBid;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedBidApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
